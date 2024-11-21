ae9c3074cb2103f0e8d02b2651dcd63977e495d7
Marika Ota <marika.oota@azoom.jp>
2024-11-21 11:58:35 +0900

    add node_module

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// main.js
+import { createApp } from 'vue'        // Vue 3の場合
+import App from './App.vue'            // ルートコンポーネントをインポート
+
+createApp(App).mount('#app')           // '#app'はマウント先のDOM要素
