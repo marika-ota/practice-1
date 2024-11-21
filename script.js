aaff643a7c4cbfcc7be4067ad580d7759e870e27
Marika Ota <marika.oota@azoom.jp>
2024-11-21 12:01:14 +0900

    :sparkles:

--- script.js
+++ script.js
@@ -7,10 +7,26 @@ const app=({
             isActive: true,
             isLarge: true,
             activeColor: 'red',
-            fontSize: 16
+            fontSize: 16,
+            count: 0,
+            doubleCount: 0,
         }
     },
-
+    methods: {
+        increment() {
+          this.count++;
+        }
+    },
+    computed: {
+        doubled(){
+            return this.count * 2;
+        }
+    },
+    watch: {
+        count(newValue) {
+            this.doubleCount = newValue * 2;
+            }
+    },
     mounted(){
         this.link = 'https://www.notion.so/13814df6f0c080e2b38ad5e3b3905b02?v=2ad427346f3e40478e381474b478ded6'
     }
